c28cc0ef0d4796fc5fcd4d9aaafbb0bc3d6e63e6
Trygve Aasen <trygve.aasen@gmail.com>
2023-02-07 20:36:33 +0100

    Added files

---
+++ DoNotDie/App.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace DoNotDie
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : System.Windows.Application
+    {
+        NotifyIcon icon = new NotifyIcon();
+        MenuItem keepAlive = new MenuItem("Keep alive");
+        MenuItem die = new MenuItem("Let die");
+        public App() 
+        {
+            var icon = new NotifyIcon();
+
+            icon.Icon = new Icon(System.Windows.Application.GetResourceStream(new Uri("pack://application:,,,/spy.ico")).Stream);
+            icon.ContextMenu = new ContextMenu();
+            keepAlive.Visible = false;
+            keepAlive.Click += KeepAlive_Click;
+
+            die.Click += Die_Click;
+            icon.ContextMenu.MenuItems.Add(keepAlive);
+            icon.ContextMenu.MenuItems.Add(die);
+            icon.Visible = true;
+
+
+            UpdateVisibility();
+            App.Current.Startup += Current_Startup;
+        }
+
+        private void Current_Startup(object sender, StartupEventArgs e)
+        {
+            Live(true);
+        }
+
+        private void Die_Click(object sender, EventArgs e)
+        {
+            Live(false);
+        }
+
+        private void KeepAlive_Click(object sender, EventArgs e)
+        {
+            Live(true);
+        }
+
+        void Live(bool live)
+        {
+            if (live)
+            {
+                ThreadState.KeepAlive();
+            }
+            else
+            {
+                ThreadState.LetDie();
+            }
+            UpdateVisibility();
+        }
+
+        void UpdateVisibility()
+        {
+            keepAlive.Visible = !ThreadState.StayingAlive;
+            die.Visible = ThreadState.StayingAlive;
+        }
+    }
+}
---
+++ DoNotDie/ThreadState.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DoNotDie
+{
+
+    public static class ThreadState
+    {
+        [DllImport("kernel32.dll")]
+        static extern uint SetThreadExecutionState(uint esFlags);
+        const uint ES_CONTINOUS = 0x80000000;
+        const uint ES_SYSTEM_REQUIRED = 0x000000001;
+        const uint ES_DISPLAY_REQUIRED = 0x00000002;
+
+        public static bool StayingAlive { get; set; }
+
+        public static void KeepAlive()
+        {
+            SetThreadExecutionState(ES_CONTINOUS | ES_DISPLAY_REQUIRED | ES_SYSTEM_REQUIRED);
+            StayingAlive = true;
+        }
+        public static void LetDie()
+        {
+            SetThreadExecutionState(ES_CONTINOUS);
+            StayingAlive = false;
+        }
+
+    }
+}
